e4d9879f4d88a696ec8cd859af4f86fce733c6c2
Chad Crowell <chadmcrowell@gmail.com>
2018-04-05 13:37:51 -0500

    add IG and route tables

--- main.tf
+++ main.tf
@@ -63,3 +63,80 @@ resource "aws_vpc" "wp_vpc" {
     Name = "wp_vpc"
   }
 }
+
+#internet gateway
+
+resource "aws_internet_gateway" "wp_internet_gateway" {
+  vpc_id = "${aws_vpc.wp_vpc.id}"
+
+  tags {
+    Name = "wp_igw"
+  }
+}
+
+# Route tables
+
+resource "aws_route_table" "wp_public_rt" {
+  vpc_id = "${aws_vpc.wp_vpc.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.wp_internet_gateway.id}"
+  }
+
+  tags {
+    Name = "wp_public"
+  }
+}
+
+resource "aws_default_route_table" "wp_private_rt" {
+  default_route_table_id = "${aws_vpc.wp_vpc.default_route_table_id}"
+
+  tags {
+    Name = "wp_private"
+  }
+}
+
+resource "aws_subnet" "wp_public1_subnet" {
+  vpc_id                  = "${aws_vpc.wp_vpc.id}"
+  cidr_block              = "${var.cidrs["public1"]}"
+  map_public_ip_on_launch = true
+  availability_zone       = "${data.aws_availability_zones.available.names[0]}"
+
+  tags {
+    Name = "wp_public1"
+  }
+}
+
+resource "aws_subnet" "wp_public2_subnet" {
+  vpc_id                  = "${aws_vpc.wp_vpc.id}"
+  cidr_block              = "${var.cidrs["public2"]}"
+  map_public_ip_on_launch = true
+  availability_zone       = "${data.aws_availability_zones.available.names[1]}"
+
+  tags {
+    Name = "wp_public2"
+  }
+}
+
+resource "aws_subnet" "wp_private1_subnet" {
+  vpc_id                  = "${aws_vpc.wp_vpc.id}"
+  cidr_block              = "${var.cidrs["private1"]}"
+  map_public_ip_on_launch = false
+  availability_zone       = "${data.aws_availability_zones.available.names[0]}"
+
+  tags {
+    Name = "wp_private1"
+  }
+}
+
+resource "aws_subnet" "wp_private2_subnet" {
+  vpc_id                  = "${aws_vpc.wp_vpc.id}"
+  cidr_block              = "${var.cidrs["private2"]}"
+  map_public_ip_on_launch = false
+  availability_zone       = "${data.aws_availability_zones.available.names[1]}"
+
+  tags {
+    Name = "wp_private2"
+  }
+}
